49d171e83e5c9418c7bec9c637ef677966a23dd4
shafira203 <shafiraakbarizkii>
2025-05-17 00:34:44 +0700

    komen pertama

---
+++ laravel/app/Models/Post.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    //
+    protected $fillable = ['title', 'slug', 'excerpt', 'content', 
+                            'published', 'published_at', 'user_id'];
+
+    public function categories()
+    
+    {
+        return $this->belongsToMany(Category::class);
+    }
+
+    public function tags()
+    
+    {
+        return $this->belongsToMany(Tag::class);
+    }
+}
