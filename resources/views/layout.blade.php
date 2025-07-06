cd91a5e2ed88776ed6168f0d9210c7bb90cffbbc
shafira203 <shafiraakbarizkii@gmail.com>
2025-07-06 21:18:43 +0700

    up login

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -23,7 +23,7 @@
             <a class="mr-5 hover:text-gray-900" href= "{{route('artikel')}}">Artikel</a>
             <a class="mr-5 hover:text-gray-900" href= "{{route('penulis')}}">Author</a>
             </nav>
-            <a href="praktekfilapab-production.up.railway.app/admin/login" class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Login
+            <a href="https://praktekfilapab-production.up.railway.app/admin/login" class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Login
             <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-1" viewBox="0 0 24 24">
                 <path d="M5 12h14M12 5l7 7-7 7"></path>
             </svg>
